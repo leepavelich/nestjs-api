60bc9589d71ef89b9ea7e82c30ef050c4c7bb674
leepavelich <leepavelich@gmail.com>
2024-03-05 19:22:51 -0700

    routes init

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable({})
+export class AuthService {
+  signup() {
+    return { msg: 'I have signed up' };
+  }
+
+  singin() {
+    return { msg: 'I have signed in' };
+  }
+}
